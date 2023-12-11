a56cd4db00b2344b9f348016611ed7e6e6ebd746
Harlam Alvarado <harlamalvarado@Harlams-MacBook-Pro.local>
2023-12-10 20:15:07 -0500

    move index file for testin3g

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+console.log("Current Working Directory:", process.cwd()); 
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
